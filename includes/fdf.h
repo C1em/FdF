93408e6feacd476eee348052625b5754116723d2
C1em <martin.corentin98@gmail.com>
2019-02-05 07:55:11 +0100

    norm

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,72 +6,72 @@
 /*   By: coremart <coremart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/19 00:48:33 by coremart          #+#    #+#             */
-/*   Updated: 2019/02/05 06:36:01 by coremart         ###   ########.fr       */
+/*   Updated: 2019/02/05 07:54:40 by coremart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _FDF_H_
-#define _FDF_H_
+#ifndef FDF_H
+# define FDF_H
 
-#define LENGHT 1500.0f
-#define HEIGHT 1000.0f
-#define PI 3.14159265359f
+# define LENGHT 1500.0f
+# define HEIGHT 1000.0f
+# define PI 3.14159265359f
 
-#include <string.h>
+# include <string.h>
 
-typedef struct  s_points
+typedef struct	s_points
 {
 	int x1;
 	int y1;
 	int x2;
 	int y2;
-}               t_points;
+}				t_points;
 
-typedef struct  s_img_info
+typedef struct	s_img_info
 {
-	void *img_ptr;
-	char *img_str;
-	int bpp;
-	int len;
-	int endian;
-}               t_img_info;
+	void	*img_ptr;
+	char	*img_str;
+	int		bpp;
+	int		len;
+	int		endian;
+}				t_img_info;
 
-typedef struct  s_vector
+typedef struct	s_vector
 {
-	float x;
-	float y;
-	float z;
-	int color;
-}               t_vector;
+	float	x;
+	float	y;
+	float	z;
+	int		color;
+}				t_vector;
 
 typedef struct	s_rot
 {
-	float teta;
-	t_vector *vec;
+	float		teta;
+	t_vector	*vec;
 }				t_rot;
 
-typedef struct s_param
+typedef struct	s_param
 {
-	int *zoom;
-	char axe;
-	int sign;
+	int		*zoom;
+	char	axe;
+	int		sign;
 }				t_param;
 
-typedef struct  s_vector_tab
+typedef struct	s_vector_tab
 {
-	t_vector **tab;
-	int nb_lines;
-	int nb_col;
-	int zoom;
-	int color;
-}               t_vector_tab;
+	t_vector	**tab;
+	int			nb_lines;
+	int			nb_col;
+	int			zoom;
+	int			color;
+}				t_vector_tab;
 
-typedef struct  s_matrice
+typedef struct	s_matrice
 {
-	int *line;
-	size_t len;
-	struct s_matrice *next;
-}               t_matrice;
+	int					*line;
+	size_t				len;
+	struct s_matrice	*next;
+}				t_matrice;
 
 typedef struct	s_mouse
 {
@@ -82,25 +82,25 @@ typedef struct	s_mouse
 	int pressed;
 }				t_mouse;
 
-typedef struct  s_data
+typedef struct	s_data
 {
-	int menu_open;
-	void *mlx_ptr;
-	void *win_ptr;
-	t_matrice *matrix;
-	t_img_info *img_info;
-	t_points *points;
-	t_vector_tab *tab;
-	t_mouse *mouse;
-}               t_data;
+	int				menu_open;
+	void			*mlx_ptr;
+	void			*win_ptr;
+	t_matrice		*matrix;
+	t_img_info		*img_info;
+	t_points		*points;
+	t_vector_tab	*tab;
+	t_mouse			*mouse;
+}				t_data;
 
-t_matrice   	*fill_map(char * path);
-int         	key_events(int keycode);
-int         	ft_open_window(void);
-t_vector_tab    *convert_to_vector(t_matrice *matrix);
+t_matrice		*fill_map(char *path);
+int				key_events(int keycode);
+int				ft_open_window(void);
+t_vector_tab	*convert_to_vector(t_matrice *matrix);
 void			rot_matrix(t_vector_tab *tab, float x, float y);
-void    		draw(t_data *data);
-int     		key_press(int key, t_data *data);
+void			draw(t_data *data);
+int				key_press(int key, t_data *data);
 void			print_menu(t_data *data);
 void			foreach_vec(t_vector_tab *tab, void (*fun)(), void *param);
 int				mouse_press(int button, int x, int y, t_mouse *mouse);
--- sources/convert_to_vector.c
+++ sources/convert_to_vector.c
@@ -6,7 +6,7 @@
 /*   By: coremart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 18:30:32 by coremart          #+#    #+#             */
-/*   Updated: 2019/02/05 07:23:47 by coremart         ###   ########.fr       */
+/*   Updated: 2019/02/05 07:45:26 by coremart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdlib.h>
 #include <errno.h>
 
-static void		find_mid(t_matrice *matrix, float *mid_x,
+static void			find_mid(t_matrice *matrix, float *mid_x,
 									float *mid_y, int *zoom)
 {
 	size_t y;
@@ -31,7 +31,7 @@ static void		find_mid(t_matrice *matrix, float *mid_x,
 	*mid_y = y / 2;
 }
 
-static size_t	count_line(t_matrice *matrix)
+static size_t		count_line(t_matrice *matrix)
 {
 	size_t i;
 
@@ -47,10 +47,12 @@ static size_t	count_line(t_matrice *matrix)
 static t_vector_tab	*new_vector_tab(t_matrice *matrice,
 								float *mid_x, float *mid_y)
 {
+	t_vector_tab *vector_tab;
+
 	if (!(vector_tab = (t_vector_tab*)malloc(sizeof(t_vector_tab))))
 		ft_error(errno);
 	find_mid(matrice, mid_x, mid_y, &vector_tab->zoom);
-	vector_tab->nb_lines = count_line(matrix);
+	vector_tab->nb_lines = count_line(matrice);
 	vector_tab->nb_col = matrice->len;
 	if (!(vector_tab->tab = (t_vector**)malloc(sizeof(t_vector*) *
 											vector_tab->nb_lines)))
@@ -58,36 +60,31 @@ static t_vector_tab	*new_vector_tab(t_matrice *matrice,
 	vector_tab->color = 0xFFFFFF;
 }
 
-t_vector_tab	*convert_to_vector(t_matrice *matrix)
+t_vector_tab		*convert_to_vector(t_matrice *matrix)
 {
 	float			mid_x;
 	float			mid_y;
 	size_t			j;
 	int				i;
-	t_vector_tab	*vector_tab;
+	t_vector_tab	*v_tab;
 
-	vector_tab = new_vector(matrix, &mid_x, &mid_y);
+	v_tab = new_vector_tab(matrix, &mid_x, &mid_y);
 	i = 0;
-	while (i < vector_tab->nb_lines)
+	while (i < v_tab->nb_lines)
 	{
-		j = 0;
-		if (!(vector_tab->tab[i] =
-				(t_vector*)malloc(sizeof(t_vector) * matrix->len)))
+		j = -1;
+		if (!(v_tab->tab[i] = (t_vector*)malloc(sizeof(t_vector) *
+			matrix->len)))
 			ft_error(errno);
-		while (j < matrix->len)
+		while (++j < matrix->len)
 		{
-			vector_tab->tab[i][j].x =
-			(float)vector_tab->zoom * ((float)j - mid_x);
-			vector_tab->tab[i][j].y =
-			(float)vector_tab->zoom * ((float)i - mid_y);
-			vector_tab->tab[i][j].z =
-			matrix->line[j] * (vector_tab->zoom);
-			vector_tab->tab[i][j].color =
-			(matrix->line[j]) ? 0xFFFFFF : 0x000000;
-			j++;
+			v_tab->tab[i][j].x = (float)v_tab->zoom * ((float)j - mid_x);
+			v_tab->tab[i][j].y = (float)v_tab->zoom * ((float)i - mid_y);
+			v_tab->tab[i][j].z = matrix->line[j] * (v_tab->zoom);
+			v_tab->tab[i][j].color = (matrix->line[j]) ? 0xFFFFFF : 0x000000;
 		}
 		matrix = matrix->next;
 		i++;
 	}
-	return (vector_tab);
+	return (v_tab);
 }
